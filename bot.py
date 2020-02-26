3b21126e9ccf8923138212434d5a209f759bbcf3
Kevin Koster <koster.k2001@gmail.com>
2020-02-26 16:00:28 +0100

    + automatic cog loader

--- bot.py
+++ bot.py
@@ -1,17 +1,33 @@
 import discord
+from discord.ext import commands
 import sys
+import cogs
+
+client = commands.Bot(command_prefix='.')
 
 
-class BitBoi(discord.Client):
-    async def on_ready(self):
-        game = discord.Game('with bits and bytes')
-        print('Logged in as', self.user)
-        await self.change_presence(status=discord.Status.online, activity=game)
+@client.event
+async def on_connect():
+    try:
+        for cog in cogs:
+            client.load_extension(cog)
+    except Exception:
+        raise
 
 
-client = BitBoi()
+@client.event
+async def on_ready():
+    game = discord.Game('with bits and bytes')
+    print('Logged in as', client.user)
+    await client.change_presence(status=discord.Status.online, activity=game)
 
-if sys.argv[1]:
-    client.run(sys.argv[1])
+
+__token__ = sys.argv[1]
+
+if __token__:
+    try:
+        client.run(__token__)
+    except discord.errors.LoginFailure:
+        raise
 else:
     raise ValueError()
