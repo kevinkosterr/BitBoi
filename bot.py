a00523ca2cd720ed3cc1732aa9ac7761c078ec10
Kevin Koster <koster.k2001@gmail.com>
2020-02-26 16:44:40 +0100

    + reload command, can now reload cogs without having to restart bot

--- bot.py
+++ bot.py
@@ -3,31 +3,48 @@ from discord.ext import commands
 import sys
 import cogs
 
-client = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='.')
 
 
-@client.event
+@bot.event
 async def on_connect():
     try:
+        # loads every cog inside the cogs folder
         for cog in cogs:
-            client.load_extension(cog)
+            bot.load_extension(cog)
+    # if any exception occurs, raise it
     except Exception:
         raise
 
 
-@client.event
+@bot.event
 async def on_ready():
     game = discord.Game('with bits and bytes')
-    print('Logged in as', client.user)
-    await client.change_presence(status=discord.Status.online, activity=game)
+    # prints the name of the bot
+    print('Logged in as', bot.user)
+    # changes the presence of the bot
+    await bot.change_presence(status=discord.Status.online, activity=game)
 
 
-__token__ = sys.argv[1]
+@bot.command()
+@commands.has_role('Developer')
+async def reload(ctx, message_id=discord.Message.id):
+    for cog in cogs:
+        bot.reload_extension(cog)
+    await ctx.send('Cogs reloaded!:white_check_mark:')
 
-if __token__:
-    try:
-        client.run(__token__)
-    except discord.errors.LoginFailure:
-        raise
-else:
-    raise ValueError()
+
+if __name__ == '__main__':
+    cogs = ['cogs.BasicCommands']
+    # the token must be given as an argument
+    __token__ = sys.argv[1]
+
+    # tries to run the bot with the given token
+    # if the token is wrong, it raises an exception
+    if __token__:
+        try:
+            bot.run(__token__)
+        except discord.errors.LoginFailure:
+            raise
+    else:
+        raise ValueError()
