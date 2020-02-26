6ec23418c99178d7dc32f7bc76d561c405855672
Kevin Koster <koster.k2001@gmail.com>
2020-02-26 23:21:12 +0100

    * fixed bot deleting too few messages

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -17,7 +17,7 @@ class AdminCommands(commands.Cog):
     @commands.command(name='clear')
     async def clear_command(self, ctx, amount: int):
         await ctx.send('Clearing messages.. this may take some time.')
-        deleted = await ctx.channel.purge(limit=amount)
+        deleted = await ctx.channel.purge(limit=amount+1)
         await ctx.send(f'Deleted {len(deleted)} messages')
 
 
