eadc1675a4ef4bdc075a6521a0b55c254f5e4054
Kevin Koster <koster.k2001@gmail.com>
2020-02-26 23:18:01 +0100

    * possible fix clear command

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -17,7 +17,7 @@ class AdminCommands(commands.Cog):
     @commands.command(name='clear')
     async def clear_command(self, ctx, amount: int):
         await ctx.send('Clearing messages.. this may take some time.')
-        deleted = await ctx.purge(limit=amount)
+        deleted = await discord.TextChannel.purge(limit=amount)
         await ctx.send(f'Deleted {len(deleted)} messages')
 
 
