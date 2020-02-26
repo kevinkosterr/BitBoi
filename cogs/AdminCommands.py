ab086980813a99469ce4a6c8189e3485200c510c
Kevin Koster <koster.k2001@gmail.com>
2020-02-27 00:00:56 +0100

    + ban command added

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -14,6 +14,14 @@ class AdminCommands(commands.Cog):
         else:
             await ctx.send(f'{member}, has been kicked from the server.')
 
+    @commands.command(name='ban')
+    async def ban_command(self, ctx, member:discord.Member, reason=None):
+        await member.ban(reason=reason)
+        if reason is not None:
+            await ctx.send(f'{member}, has been banned from the server. Reason: {reason}')
+        else:
+            await ctx.send(f'{member}, has been banned from the server.')
+
     @commands.command(name='clear')
     async def clear_command(self, ctx, amount: int):
         await ctx.send('Clearing messages.. this may take some time.')
