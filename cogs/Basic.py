4eb38997c1153a5c1f1c8164b49914e9710e41b1
Kevin Koster <koster.k2001@gmail.com>
2020-02-27 20:22:48 +0100

    * removed old code

--- cogs/Basic.py
+++ cogs/Basic.py
@@ -52,39 +52,6 @@ class Basic(commands.Cog):
                         color=discord.Color.red()
                     )
                 await ctx.send(embed=help_embed)
-        # author = ctx.message.author
-        #
-        # basic_embed = discord.Embed(
-        #     color=discord.Color.blue(),
-        #     description=f'the current prefix is: {self.bot.command_prefix}'
-        # )
-        # basic_embed.set_author(name='Basic commands')
-        # basic_embed.add_field(name='.beep', value='BOOP!', inline=False)
-        # basic_embed.add_field(name='.serverinfo', value='Shows server info', inline=False)
-        #
-        # minecraft_embed = discord.Embed(
-        #     color=discord.Color.green()
-        # )
-        # minecraft_embed.set_author(name='Minecraft commands')
-        # minecraft_embed.add_field(name='.ip', value='Shows ip address of minecraft server', inline=False)
-        #
-        # admin_embed = discord.Embed(
-        #     color=discord.Color.red()
-        # )
-        # admin_embed.set_author(name='Admin commands')
-        # admin_embed.add_field(name='.kick', value='Kicks the specified member', inline=False)
-        # admin_embed.add_field(name='.ban', value='Bans the specified member', inline=False)
-        # admin_embed.add_field(name='.clear', value='Deletes messages', inline=False)
-        #
-        # admin_role = discord.utils.find(lambda r: r.name == 'Bit-Developer', ctx.message.guild.roles)
-        # if admin_role in author.roles:
-        #     await ctx.send(embed=basic_embed)
-        #
-        #     await ctx.send(embed=minecraft_embed)
-        #     await ctx.send(embed=admin_embed)
-        # else:
-        #     await ctx.send(embed=basic_embed)
-        #     await ctx.send(embed=minecraft_embed)
 
     @commands.command(name='serverinfo', aliases=['si', 's'])
     async def server_inf_command(self, ctx):
