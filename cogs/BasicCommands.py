d3385ed5d533ece22b0750038446034c6bd2c7d2
Kevin Koster <koster.k2001@gmail.com>
2020-02-27 03:04:09 +0100

    + added a basic help command

--- cogs/BasicCommands.py
+++ cogs/BasicCommands.py
@@ -6,6 +6,42 @@ class BasicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='help')
+    async def help_command(self, ctx, *cmd):
+        author = ctx.message.author
+
+        basic_embed = discord.Embed(
+            color=discord.Color.blue(),
+            description=f'the current prefix is: {self.bot.command_prefix}'
+        )
+        basic_embed.set_author(name='Basic commands')
+        basic_embed.add_field(name='.beep', value='BOOP!', inline=False)
+        basic_embed.add_field(name='.serverinfo', value='Shows server info', inline=False)
+
+        minecraft_embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        minecraft_embed.set_author(name='Minecraft commands')
+        minecraft_embed.add_field(name='.ip', value='Shows ip address of minecraft server', inline=False)
+
+        admin_embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        admin_embed.set_author(name='Admin commands')
+        admin_embed.add_field(name='.kick', value='Kicks the specified member', inline=False)
+        admin_embed.add_field(name='.ban', value='Bans the specified member', inline=False)
+        admin_embed.add_field(name='.clear', value='Deletes messages', inline=False)
+
+        admin_role = discord.utils.find(lambda r: r.name == 'Developer', ctx.message.guild.roles)
+        if admin_role in author.roles:
+            await ctx.send(embed=basic_embed)
+            await ctx.send(embed=minecraft_embed)
+            await ctx.send(embed=admin_embed)
+        else:
+            await ctx.send(embed=basic_embed)
+            await ctx.send(embed=minecraft_embed)
+
+
     @commands.command(name='beep')
     async def beep_command(self, ctx):
         await ctx.send('Boop!:robot:')
