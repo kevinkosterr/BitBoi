7ac6012d399c5fdebaf6b786640e784a5303e28f
Kevin Koster <koster.k2001@gmail.com>
2020-03-04 11:40:45 +0100

    + gif command

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,5 +1,16 @@
+import discord
 from discord.ext import commands
+
 import random
+import aiohttp
+import json
+import toml
+
+
+def load_config(subj: str, part: str):
+    """Loads the configuration"""
+    config = toml.load('config.toml').get(subj).get(part)
+    return config
 
 
 class Fun(commands.Cog):
@@ -28,6 +39,30 @@ class Fun(commands.Cog):
                    "you look like two of the ugliest animals merged together."
                    ]
         await ctx.send(f'{author.mention}, {random.choice(insults)}')
+
+    @commands.command(name='gif')
+    async def gif_command(self, ctx, *search: str):
+        """sends a random GIF"""
+        embed = discord.Embed(color=discord.Color.green())
+        api_key = load_config('GIPHY', 'api_key')
+        async with aiohttp.ClientSession() as session:
+            if not search:
+                response = await session.get(f'https://api.giphy.com/v1/gifs/random?api_key={api_key}')
+                # loads the response data as json
+                data = json.loads(await response.text())
+                # sets the embed image to the
+                # image from the response
+                embed.set_image(url=data['data']['images']['original']['url'])
+            else:
+                response = await session.get(f'https://api.giphy.com/v1/gifs/search?q={search}&api_key={api_key}&limit=10')
+                # loads the response data as json
+                data = json.loads(await response.text())
+                # chooses a random gif from the GIFS found by GIPHY
+                gif_choice = random.randint(0, 9)
+                # sets the embed image to the
+                # image from the response
+                embed.set_image(url=data['data'][gif_choice]['images']['original']['url'])
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
