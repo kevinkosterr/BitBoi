d5dc97315bb86a805b9e29394223316d7ca9b671
Jesper-Creator <jtb2003@DC13>
2020-03-19 23:03:51 +0100

    Changed .ip command from minecraftsurvival.nl to play.minecraftsurvival.nl
    changed the .mcstatus command host from minecraftsurvival.nl to play.minecraftsurvival.nl

--- cogs/Minecraft.py
+++ cogs/Minecraft.py
@@ -12,7 +12,7 @@ class Minecraft(commands.Cog):
     @commands.command(name='ip')
     async def ip_command(self, ctx):
         """gives the ip address of the server"""
-        await ctx.send('minecraftsurvival.nl:cactus:')
+        await ctx.send('play.minecraftsurvival.nl:cactus:')
 
     @commands.command(name='mcstatus')
     async def mcstatus_command(self, ctx):
@@ -29,7 +29,7 @@ class Minecraft(commands.Cog):
         """
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
-        host = "minecraftsurvival.nl"
+        host = "play.minecraftsurvival.nl"
         port = 25565
         if s.connect_ex((host, port)):
             return False
