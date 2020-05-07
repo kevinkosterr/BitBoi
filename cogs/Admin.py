4ce7030cc786186f087f38f65ccd2a07e859e533
Kevin Koster <koster.k2001@gmail.com>
2020-05-07 23:12:24 +0200

    + basic logging in commandline when a command is used. (planning on adding proper logging later)

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -15,8 +15,10 @@ class Admin(commands.Cog):
         """.kick <user> <reason>, kicks the specified user"""
         await member.kick(reason=reason)
         if reason is not None:
+            print(f'{ctx.message.author} kicked {member}: {reason}')
             await ctx.send(f'{member}, has been kicked from the server. Reason: {reason}')
         else:
+            print(f'{ctx.message.author} kicked {member}. UNKNOWN REASON')
             await ctx.send(f'{member}, has been kicked from the server.')
 
     @commands.command(name='ban')
@@ -25,8 +27,10 @@ class Admin(commands.Cog):
         """.ban <user> <reason>, bans the specified user"""
         await member.ban(reason=reason)
         if reason is not None:
+            print(f'{ctx.message.author} has banned {member}: {reason}')
             await ctx.send(f'{member}, has been banned from the server. Reason: {reason}')
         else:
+            print(f'{ctx.message.author} has banned {member}. REASON UNKNOWN')
             await ctx.send(f'{member}, has been banned from the server.')
 
     @commands.command(name='clear')
@@ -34,6 +38,7 @@ class Admin(commands.Cog):
     async def clear_command(self, ctx, amount: int):
         """.clear <amount>, clears the amount of messages specified"""
         deleted = await ctx.channel.purge(limit=amount + 1)
+        print(f'{ctx.message.author} has cleared {amount} messages.')
         delete_msg = await ctx.send(f'{len(deleted) - 1} messages have been deleted.')
         time.sleep(1)
         await discord.Message.delete(delete_msg)
